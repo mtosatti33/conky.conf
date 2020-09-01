7f2bb99b50c8e0668a467de49758f8ee18685b5d
mtosatti33 <m_tosatti@yahoo.com.br>
2020-08-31 21:32:35 -0300

    Script para configuração do conky

--- config.sh
+++ config.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+
+CWD=$(pwd)
+
 #usa-se o sed pra modificar o adaptador da conexão
+if [ ! -e ~/.config/conky ]; then
+    mkdir ~/.config/conky
+fi
+
+cp $CWD/.config/conky/conky.conf ~/.config/conky
+
 sed 's/wlx409bcd9656af/wlx409bcd9656af/g' ~/.config/conky/conky.conf
 
